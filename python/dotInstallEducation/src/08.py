b4119b8b69b5818bde5504bba81c4c25dc21644e
budougumi0617 <budougumi0617@gmail.com>
2013-10-04 17:01:07 +0900

    add .py file

---
+++ python/dotInstallEducation/src/08.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+# string operation
+
+s = "abcdefghi"
+
+print "s = " + s
+
+print s[1]
+
+print s[1:-2]
+
+# search string
+print s.find("cd")
+
+
---
+++ python/dotInstallEducation/src/09.py
@@ -0,0 +1,9 @@
+#encoding: UTF-8
+#caluculate string and int or float
+
+print 5 + int("5")
+
+age = 20
+
+print "i am " + str(age) + " years old"
+
